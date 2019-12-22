176d8642edbe9f3cf3e231baf72ed26003b4101d
Gabriel Pereira <gabrielrcpereira@gmail.com>
2019-12-22 19:31:38 -0300

    Improving and adding new tests

--- src/test/java/com/grcp/weather/rhythm/api/service/WeatherRhythmTestService.java
+++ src/test/java/com/grcp/weather/rhythm/api/service/WeatherRhythmTestService.java
@@ -10,6 +10,8 @@ import java.io.IOException;
 import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.mockito.Mock;
 import static org.mockito.Mockito.when;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -31,11 +33,12 @@ public class WeatherRhythmTestService {
         service = new WeatherRhythmService(weatherHelper, musicHelper);
     }
 
-    @Test
-    public void retrieveRhythmByCityName_whenTemperatureIsAboveOfThirty() throws IOException, SpotifyWebApiException {
+    @ParameterizedTest
+    @ValueSource(doubles = {31.0, 32.0, 40.0, 50.0})
+    public void retrieveRhythmByCityName_whenTemperatureIsAboveOfThirty_expectedPartyMusic(double temperature) throws IOException, SpotifyWebApiException {
         // given
         String cityName = "cityNameTest";
-        MainResponse mainResponse = MainResponse.builder().temp(31).build();
+        MainResponse mainResponse = MainResponse.builder().temp(temperature).build();
         WeatherApiResponse weatherApiResponse = WeatherApiResponse.builder().main(mainResponse).build();
 
         // when
@@ -47,16 +50,34 @@ public class WeatherRhythmTestService {
         Assert.notEmpty(response.getMusics(), "Expected a list filled with at least one party music.");
     }
 
-    @Test
-    public void retrieveRhythmByCityName_whenGotTemperatureFifteen() throws IOException, SpotifyWebApiException {
+    @ParameterizedTest
+    @ValueSource(doubles = {15.0, 16.0, 17.0, 21.0, 22.0, 23.0, 24.0, 28.0, 29.0, 30.0})
+    public void retrieveRhythmByCityName_whenGetTemperatureIsBetweenFifteenAndThirty_expectedPopMusic(double temperature) throws IOException, SpotifyWebApiException {
         // given
         String cityName = "cityNameTest";
-        MainResponse mainResponse = MainResponse.builder().temp(15).build();
+        MainResponse mainResponse = MainResponse.builder().temp(temperature).build();
         WeatherApiResponse weatherApiResponse = WeatherApiResponse.builder().main(mainResponse).build();
 
         // when
         when(weatherHelper.getCurrentWeatherByCityName(cityName)).thenReturn(weatherApiResponse);
         when(musicHelper.retrieveMusicsByPopCategory()).thenReturn(List.of(MusicResponse.builder().build()));
+
+        // then
+        WeatherRhythmResponse response = service.retrieveRhythmsByCityName(cityName);
+        Assert.notEmpty(response.getMusics(), "Expected a list filled with at least one party music.");
+    }
+
+    @ParameterizedTest
+    @ValueSource(doubles = {10.0, 11.0, 12.0, 13.0, 14.0})
+    public void retrieveRhythmByCityName_whenTemperatureIsBetweenTenAndFourteen_expectedRockMusic(double temperature) throws IOException, SpotifyWebApiException {
+        // given
+        String cityName = "cityNameTest";
+        MainResponse mainResponse = MainResponse.builder().temp(temperature).build();
+        WeatherApiResponse weatherApiResponse = WeatherApiResponse.builder().main(mainResponse).build();
+
+        // when
+        when(weatherHelper.getCurrentWeatherByCityName(cityName)).thenReturn(weatherApiResponse);
+        when(musicHelper.retrieveMusicsByRockCategory()).thenReturn(List.of(MusicResponse.builder().build()));
 
         // then
         WeatherRhythmResponse response = service.retrieveRhythmsByCityName(cityName);
