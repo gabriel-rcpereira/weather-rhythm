b65df0526f77754e6249d41a19f29867774bf999
Gabriel Pereira <gabrielrcpereira@gmail.com>
2021-02-14 13:09:13 -0300

    updating dependency manager to maven

--- clean-arch/src/main/java/com/grcp/weatherrhythm/localsong/config/openapi/SpringDoc.java
+++ clean-arch/src/main/java/com/grcp/weatherrhythm/localsong/config/openapi/SpringDoc.java
@@ -3,7 +3,6 @@ package com.grcp.weatherrhythm.localsong.config.openapi;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.info.BuildProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -22,5 +21,4 @@ public class SpringDoc {
                         .description("The APIs return Playlist Songs by you local data")
                         .version(buildProperties.getVersion()));
     }
-
 }
